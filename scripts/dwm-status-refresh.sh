1071befe59e29382d35e31784c6fd92a002db1ba
刘军 <liujun@gw.com.cn>
2021-12-15 19:50:49 +0800

    bar

--- scripts/dwm-status-refresh.sh
+++ scripts/dwm-status-refresh.sh
@@ -100,37 +100,54 @@ export IDENTIFIER="unicode"
 #. "$DIR/dwmbar-functions/dwm_vpn.sh"
 #. "$DIR/dwmbar-functions/dwm_weather.sh"
 
+# This function parses /proc/net/dev file searching for a line containing $interface data.
+# Within that line, the first and ninth numbers after ':' are respectively the received and transmited bytes.
+function get_bytes {
+	# Find active network interface
+	interface=$(ip route get 8.8.8.8 2>/dev/null| awk '{print $5}')
+	line=$(grep $interface /proc/net/dev | cut -d ':' -f 2 | awk '{print "received_bytes="$1, "transmitted_bytes="$9}')
+	eval $line
+	now=$(date +%s%N)
+}
+
+# Function which calculates the speed using actual and old byte number.
+# Speed is shown in KByte per second when greater or equal than 1 KByte per second.
+# This function should be called each second.
+function get_velocity {
+	value=$1
+	old_value=$2
+	now=$3
+
+	timediff=$(($now - $old_time))
+	velKB=$(echo "1000000000*($value-$old_value)/1024/$timediff" | bc)
+	if test "$velKB" -gt 1024
+	then
+		echo $(echo "scale=2; $velKB/1024" | bc)MB/s
+	else
+		echo ${velKB}KB/s
+	fi
+}
+
+# Get initial values
+get_bytes
+old_received_bytes=$received_bytes
+old_transmitted_bytes=$transmitted_bytes
+old_time=$now
+
+#!/bin/bash
+get_bytes
+
+# Calculates speeds
+vel_recv=$(get_velocity $received_bytes $old_received_bytes $now)
+vel_trans=$(get_velocity $transmitted_bytes $old_transmitted_bytes $now)
+
+# Update old values to perform new calculations
+old_received_bytes=$received_bytes
+old_transmitted_bytes=$transmitted_bytes
+old_time=$now
+
 # xsetroot -name "$(dwm_network) $(dwm_cpu)  $(print_mem)  ^b#111111^  $(print_bat)  $(print_temp)  $(dwm_pulse)  $(get_backlight)   ^d^   [$(print_date)^d^]"
-xsetroot -name "^d^ $(dwm_cpu) ^d^ $(print_mem) ^d^ $(print_temp) ^d^ [$(print_date)]"
+xsetroot -name "[$MEMCOL⬇️ $TEXTCOL$vel_recv $MEMCOL⬆️ $TEXTCOL$vel_trans^d^] $(dwm_cpu) ^d^ $(print_mem) ^d^ $(print_temp) ^d^ [$(print_date)]"
 
 exit 0
-
-# print_volume() {
-#   volume="$(amixer get Master | tail -n1 | sed -r 's/.*\[(.*)%\].*/\1/')"
-#   if test "$volume" -gt 0
-#   then
-#       echo -e "\uE05D${volume}"
-#   else
-#       echo -e "Mute"
-#   fi
-# }
-
-# show_record(){
-#   test -f /tmp/r2d2 || return
-#   rp=$(cat /tmp/r2d2 | awk '{print $2}')
-#   size=$(du -h $rp | awk '{print $1}')
-#   echo " $size $(basename $rp)"
-# }
-
-# get_time_until_charged() {
-#   # parses acpitool's battery info for the remaining charge of all batteries and sums them up
-#   sum_remaining_charge=$(acpitool -B | grep -E 'Remaining capacity' | awk '{print $4}' | grep -Eo "[0-9]+" | paste -sd+ | bc);
-#   # finds the rate at which the batteries being drained at
-#   present_rate=$(acpitool -B | grep -E 'Present rate' | awk '{print $4}' | grep -Eo "[0-9]+" | paste -sd+ | bc);
-#   # divides current charge by the rate at which it's falling, then converts it into seconds for `date`
-#   seconds=$(bc <<< "scale = 10; ($sum_remaining_charge / $present_rate) * 3600");
-#   # prettifies the seconds into h:mm:ss format
-#   pretty_time=$(date -u -d @${seconds} +%T);
-#   echo $pretty_time;
-# }
 
